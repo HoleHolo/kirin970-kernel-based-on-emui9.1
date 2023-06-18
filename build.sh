04f1928fb6b2f0efb0d8b2f19170407a8841521b
HoleHolo <80876542+HoleHolo@users.noreply.github.com>
2023-06-18 21:59:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,11 @@
 
 export ARCH=arm64
 
-export PATH=$PATH:/home/user/kernel/gcc/bin
-export CROSS_COMPILE=aarch64-linux-android-
-
+# 请自行修改交叉编译器路径及名称
 export PATH=$PATH:/home/user/kernel/10.3/bin
 export CROSS_COMPILE=aarch64-linux-android-
 
 mkdir ../out
 
 make ARCH=arm64 O=../out merge_kirin970_defconfig
-make ARCH=arm64 O=../out -j4
+make ARCH=arm64 O=../out -j$(($(nproc) * 2))
