46031a9be72f4b37468346f334d1f2a25e9b9417
HoleHolo <hola0_o@qq.com>
2023-12-30 11:15:46 +0800

    config_gui.sh: create file

---
+++ config_gui.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export ARCH=arm64
+
+# 请自行修改交叉编译器路径及名称
+export PATH=$PATH:~/ToolChains/bin
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+
+# 填写你自己的主机名
+export KBUILD_BUILD_HOST=PC
+
+make O=../OUT nconfig
