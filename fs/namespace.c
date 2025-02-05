555525b042ca592904b3d48173db3ce103bc51b9
HoleHolo <hola0_o@qq.com>
2025-02-05 17:55:28 +0800

    chores: Reset manual patch

--- fs/namespace.c
+++ fs/namespace.c
@@ -1721,20 +1721,6 @@ static int can_umount(const struct path *path, int flags)
 		return 0;
 	}
 
-	int path_umount(struct path *path, int flags)
-	{
-			struct mount *mnt = real_mount(path->mnt);
-			int ret;
-
-			ret = can_umount(path, flags);
-			if (!ret)
-				ret = do_umount(mnt, flags);
-
-			/* we mustn't call path_put() as that would clear mnt_expiry_mark */
-			dput(path->dentry);
-			mntput_no_expire(mnt);
-			return ret;
-		}
 
 /*
  * Now umount can handle mount points as well as block devices.
